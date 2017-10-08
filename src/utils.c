54b0affd479afe0c15d8952f38cef35bc5f31701
201420958 <sanyu1995@naver.com>
2017-10-08 19:39:59 +0900

    my first parsing

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,44 @@
 #include "utils.h"
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+char* com_tmp=(char*)malloc(sizeof(char)*255);
+char* tok;
+int i =0;
+
+strcpy(com_tmp,command);
+int len=strlen(com_tmp);
+for(int j=len+1;j>=0;j--)
+{
+if(*(com_tmp+j) == '\n')
+{
+*(com_tmp+j) = '\0';
+break;
+}
+}
+
+tok=strtok(com_tmp," \n\t");
+*argv = (char**)malloc(sizeof(char)*255);
+
+if(tok == NULL)
+{
+i++;
+*argc=i;
+*(*argv)=(char*)malloc(sizeof(char)*255);
+strcpy(*(*argv),"");
+return;
+}
+while(tok!=NULL)
+{
+*(*argv+i)=(char*)malloc(sizeof(char)*255);
+strcpy(*(*argv+i),tok);
+tok=strtok(NULL," \n\t");
+i++;
+*argc=i;
+}
+return;
 }
