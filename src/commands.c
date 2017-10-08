bd1983fecdb1a1a1d96c3a41f2d595c6ba7331b8
201420958 <sanyu1995@naver.com>
2017-10-08 19:46:31 +0900

    my commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,38 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+ else{ chdir(argv[1]);
   return 0;
+ }
 }
+
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+else{
+  char buf[8096];
+getcwd(buf,8096);
+printf("%s\n",buf);
   return 0;
+}
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+ if(argc==2&&strcmp((*argv+0),"cd")==0){
+if(chdir(argv[1]==0))
   return 1;
+}
+else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc==1&&strcmp((*argv+0),"pwd")==0)  return 1;
+else  return 0;
 }
